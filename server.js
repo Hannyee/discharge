b4e82f670202915c32e09e8f0997f63cbfbc0d72
Hannyee <tanhannyee@hotmail.com>
2025-05-22 12:38:42 +0000

    appointment,contact

--- server.js
+++ server.js
@@ -8,9 +8,20 @@ app.set('view engine', 'ejs');
 // Needed for public directory
 app.use(express.static(__dirname + '/public'));
 
+
 // Main landing page
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('pages/index', { path: '/' });
+});
+
+// Appointments page
+app.get('/appointments', function(req, res) {
+    res.render('pages/appointments', { path: '/appointments' });
+});
+
+// Contacts page
+app.get('/contacts', function(req, res) {
+    res.render('pages/contacts', { path: '/contacts' });
 });
 
 // Tells the app which port to run on
